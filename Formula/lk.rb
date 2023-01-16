ae9194c9740c6eea7fa52efc629116cfdde5a08f
James Cole <44881720+jamescoleuk@users.noreply.github.com>
2023-01-16 09:34:52 +0000

    Fix hash

--- Formula/lk.rb
+++ Formula/lk.rb
@@ -2,7 +2,7 @@ class Lk < Formula
   desc "A command palette for your bash functions"
   homepage "https://github.com/jamescoleuk/lk"
   url "https://github.com/jamescoleuk/lk/releases/download/0.3.4/lk-Darwin-x86_64.tar.gz"
-  sha256 "340d4a6ede59af15ca62f87e827031083d12f59f"
+  sha256 "bc0e4bd93cd742ffc3f63a73a3137872e6f096ca86c9b353a6250a4f1e0eff17"
   version "0.3.4"
 
   def install
